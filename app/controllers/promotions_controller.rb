6362e1ab90966922780844b4d3b782a953f0d1a0
Raquel Hess <kelmatica@gmail.com>
2021-03-26 18:03:34 -0300

    Validação de promoção

---
+++ app/controllers/promotions_controller.rb
@@ -0,0 +1,31 @@
+class PromotionsController < ApplicationController
+  def index
+    @promotions = Promotion.all
+  end
+
+  def show
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.new(promotion_params)
+    if @promotion.save
+      redirect_to @promotion
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def promotion_params
+      params
+        .require(:promotion)
+        .permit(:name, :expiration_date, :description, 
+                :discount_rate, :code, :coupon_quantity)
+    end
+end
